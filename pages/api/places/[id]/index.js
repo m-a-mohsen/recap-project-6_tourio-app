0f77850b050724a7d8841b522a19372fd665f9d8
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-11-24 14:44:09 +0100

    comments fetching Co-authored-by: Scot-MacDonald <Scot-MacDonald@users.noreply.github.com>

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,23 +1,49 @@
-import { db_places } from "../../../../lib/db_places";
-import { db_comments } from "../../../../lib/db_comments";
+import clientPromise from "../../../../db/mongodb.ts";
+import { ObjectId } from "mongodb";
+ 
+export default async (req, res) => {
+    const { id } = req.query;
 
-export default function handler(request, response) {
-  const { id } = request.query;
+    if (!id) {
+      return;
+    }
+  try {
+    const client = await clientPromise;
+    const db = client.db("tourio-app");
 
-  if (!id) {
-    return;
+    const place = await db
+      .collection("places")
+      .findOne({ _id: new ObjectId(id) });
+    console.log(place)
+
+    const comment = place?.comments;
+    console.log(typeof comment)
+    res.json(place);
+  } catch (e) {
+    console.error(e);
   }
+};
 
-  const place = db_places.find((place) => place._id.$oid === id);
-  const comment = place?.comments;
-  const allCommentIds = comment?.map((comment) => comment.$oid) || [];
-  const comments = db_comments.filter((comment) =>
-    allCommentIds.includes(comment._id.$oid)
-  );
+// import { db_places } from "../../../../lib/db_places";
+// import { db_comments } from "../../../../lib/db_comments";
 
-  if (!place) {
-    return response.status(404).json({ status: "Not found" });
-  }
+// export default function handler(request, response) {
+//   const { id } = request.query;
 
-  response.status(200).json({ place: place, comments: comments });
-}
+//   if (!id) {
+//     return;
+//   }
+
+//   const place = db_places.find((place) => place._id.$oid === id);
+//   const comment = place?.comments;
+//   const allCommentIds = comment?.map((comment) => comment.$oid) || [];
+//   const comments = db_comments.filter((comment) =>
+//     allCommentIds.includes(comment._id.$oid)
+//   );
+
+//   if (!place) {
+//     return response.status(404).json({ status: "Not found" });
+//   }
+
+//   response.status(200).json({ place: place, comments: comments });
+// }
--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,52 @@
-import { db_places } from "../../../lib/db_places";
+// import dbConnect from "../../../db/connect";
+// import Place from "../../../db/models/Place";
+// import clientPromise from "../lib/mongodb";
 
-export default function handler(request, response) {
-  return response.status(200).json(db_places);
-}
+import clientPromise from "../../../db/mongodb";
+
+export default async (req, res) => {
+  try {
+    const client = await clientPromise;
+    const db = client.db("tourio-app");
+
+    const places = await db.collection("places").find({}).toArray();
+
+    res.json(places);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+// export default async function handler(request, response) {
+//   await dbConnect();
+//   if (request.method === 'GET') {
+//        const places = await Place.find();
+//     return response.status(200).json(places);
+//   }
+//   // return response.status(200).json(db_places);
+// }
+
+// --- scot --------
+//
+// import dbConnect from "../../../db/connect";
+// import Mix from "../../../db/models/Mix";
+
+// export default async function handler(request, response) {
+//   await dbConnect();
+
+//   if (request.method === "GET") {
+//     const mixes = await Mix.find();
+//     return response.status(200).json(mixes);
+//   }
+
+//   if (request.method === "POST") {
+//     try {
+//       const mixData = request.body;
+//       await Mix.create(mixData);
+
+//       response.status(201).json({ status: "Joke created" });
+//     } catch (error) {
+//       response.status(400).json({ error: error.message });
+//     }
+//   }
+// }
